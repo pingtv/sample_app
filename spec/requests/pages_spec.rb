b8cb4c2609c258fcb8addf4c720b679facd3ce04
pingchen <pingtv@gmail.com>
2014-07-10 17:57:15 -0700

    titles and spec stuff

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Sample App'" do
+      visit '/pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/pages/contact'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/pages/contact'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About US'" do
+      visit 'pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+end
