e405a880c2dc536ae03636088c22b058f7e7894b
pingchen <pingtv@gmail.com>
2014-07-11 13:39:34 -0700

    utilized full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title depending on if there's a specified page title
+	def full_title(page_title)
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
