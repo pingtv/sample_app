28553d42f3665e018e95d0158611e84aaa36b398
pingchen <pingtv@gmail.com>
2014-07-11 15:54:13 -0700

    no pipe on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
 
   def home
-    @title = ""
   end
 
   def contact
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	# Returns the full title depending on if there's a specified page title
 	def full_title
 		base_title = "Ruby on Rails Tutorial Sample App"
-		if @title.empty?
+		if @title.nil?
 			base_title
 		else
 			"#{base_title} | #{@title}"
