681c2f73ab1f97dfc83fbf73445d9e6f85ac6304
pingchen <pingtv@gmail.com>
2014-07-11 13:15:33 -0700

    Finished adding help and contact and passed tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -36,12 +36,12 @@ describe "Pages" do
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/pages/Help'
+      visit '/pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/pages/Help'
+      visit '/pages/help'
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
