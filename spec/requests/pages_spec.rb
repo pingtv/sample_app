521e57cdbd221d33926d0e8aa124b258fb0d1b0b
pingchen <pingtv@gmail.com>
2014-07-11 13:10:30 -0700

    added help

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -33,6 +33,19 @@ describe "Pages" do
     end
   end
 
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/pages/Help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/pages/Help'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
   describe "About page" do
 
     it "should have the h1 'About Us'" do
