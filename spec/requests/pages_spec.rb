c13db58e2abdcfcde768c8e4fb240162f6abbd0f
pingchen <pingtv@gmail.com>
2014-07-10 18:07:52 -0700

    serve_static_assets = true

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -19,12 +19,12 @@ describe "Pages" do
 
     it "should have the h1 'Contact'" do
       visit '/pages/contact'
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
       visit '/pages/contact'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
@@ -35,9 +35,9 @@ describe "Pages" do
       page.should have_selector('h1', :text => 'About Us')
     end
 
-    it "should have the title 'About US'" do
+    it "should have the title 'About Us'" do
       visit 'pages/about'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
